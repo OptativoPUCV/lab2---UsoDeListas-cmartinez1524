480edc7582afb5ba4962f807a4300d977fed7b4f
Your Name <you@example.com>
2024-03-21 02:06:19 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -134,7 +134,36 @@ paraéntesis balanceados. Retorna 1 si están balanceados,
 0 en caso contrario.
 */
 
-int parentesisBalanceados(char *cadena) {
-   return 0;
+int parentesisBalanceados(char *cadena) 
+{
+  Stack* P = create_stack();
+  int i = 0;
+  char *dato;
+  dato = (char*)malloc(sizeof(char));
+  while(cadena[i] != '\0')
+    {
+      if(cadena[i] == '(')
+      {
+        push(P, dato);
+      }
+      else
+      {
+        if(cadena[i] == ')')
+        {
+          if(pop(P) == NULL)
+          {
+            return 0;
+          }
+          
+        }
+        
+      }
+      i++;
+      
+    }
+  
+  
+  
+  return 0;
 }
 
