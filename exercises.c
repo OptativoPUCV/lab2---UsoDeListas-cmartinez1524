e9d1991d2a786d5fb87d3b7ac73347f3fd3305b4
Your Name <you@example.com>
2024-03-23 01:10:29 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -151,9 +151,9 @@ int parentesisBalanceados(char* cadena) {
                 return 0; // Hay un cierre sin una correspondiente apertura
             }
             char *topChar = (char*)pop(P);
-            if ((*dato == ')' && topChar != '(') ||
-                (*dato == ']' && topChar != '[') ||
-                (*dato == '}' && topChar != '{')) {
+            if ((*dato == ')' && *topChar != '(') ||
+                (*dato == ']' && *topChar != '[') ||
+                (*dato == '}' && *topChar != '{')) {
                 free(dato); // Liberar memoria asignada antes de salir
                 return 0; // El cierre no coincide con la apertura
             }
